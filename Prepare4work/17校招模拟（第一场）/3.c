9ad6663d95465642afb0b2329155e47210b2aff0
daibigmonster <546508148@qq.com>
2017-08-07 10:55:49 +0800

    1

--- Prepare4work/17校招模拟（第一场）/3.c
+++ Prepare4work/17校招模拟（第一场）/3.c
@@ -9,11 +9,24 @@ int len(int num)
     while(num/=10)
         length++;
     return length;}
-int count0(int num)
+int count(string num,int k)
 {
-    if(num<=9)return 0;
+    if(num.empty())return 0;}
+int count0(string num)
+{
+    int ifactor=0;
+    int length=num.size();
+    while(ifactor!=length)
+    {
+        first=num[ifactor]-'0';
+        if(length==1&&first==0)return 0;
+        //if(length==1&&first>0)return 1;
+        if(first>0)first--;
+        if()}
+    /*if(num<=9)return 0;
     int highnum,curnum,lownum;
-    int length=len(num),count=0;
+    int length=len(num),count=0,ifactor=1;
+    while(n/ifactor!=0)
     for(int i=1;highnum=num/(i*10);i*=10)
     {
         curnum=num/i;
@@ -21,12 +34,12 @@ int count0(int num)
         count+=(highnum-1)*pow(10,len(i/10))+lownum+1; 
         cout<<(highnum-1)*pow(10,len(i/10))+lownum+1<<endl;
     }
-    return count;
+    return count;*/
 }
 int main(){
     int num;
     while(cin>>num){
-    int result=count0(num);
+    int result=count0(to_string(num));
     cout<<result<<endl;}
     return 0;
 }
