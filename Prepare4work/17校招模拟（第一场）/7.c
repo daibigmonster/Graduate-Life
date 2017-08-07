fe53f116c7adbd16b6a6e0b1e039e0556c2091a8
daibigmonster <546508148@qq.com>
2017-08-07 18:24:29 +0800

    终于把问题搞定了

--- Prepare4work/17校招模拟（第一场）/7.c
+++ Prepare4work/17校招模拟（第一场）/7.c
@@ -2,51 +2,38 @@
 #include<vector>
 #include<math.h>
 using namespace std;
-int len(int num)
+int countnum(int num,int k)
 {
-    if(num==0) return 0;
-    int length=1;
-    while(num/=10)
-        length++;
-    return length;}
-int count0(int num)
-{
-    if(num<=9)return 0;
-    int highnum,curnum,lownum;
-    int length=len(num),count=0;
-    for(int i=1;highnum=num/(i*10);i*=10)
+    int count=0;
+    int factor=1;
+    int lownum=0;
+    int curnum=0;
+    int highnum=0;
+    while(num/factor){
+    lownum=num-(num/factor)*factor;
+    curnum=(num/factor)%10;
+    highnum=num/(factor*10);
+    if(k==0)
     {
-        curnum=num/i;
-        lownum=num%i;
-        count+=(highnum-1)*pow(10,len(i/10))+lownum+1; 
-       // cout<<count<<endl;
+        if(highnum)highnum--;
+        else break;
+    }
+    count+=highnum*factor;
+    if(curnum>k)
+        count+=factor;
+    else if(curnum==k)
+        count+=lownum+1;
+    factor*=10;
     }
     return count;
-}
-int count(int num,int k)
-{
-    if(num==0)return 0;
-    int length=len(num);
-    int first=static_cast<int>(num/pow(10,length-1));
-    int firstcount; 
-    if(first==k)
-        firstcount=num-first*pow(10,length-1)+1;
-    else if(first>k)
-        firstcount=pow(10,length-1);
-    else
-        firstcount=0;
-    int notfirstcount=(length-1)*pow(10,length-2)*first;
-    int lastcount=count(num-first*pow(10,length-1),k);
-    return firstcount+notfirstcount+lastcount;
-}
-int main()
-{
-    int num;
     
+}
+
+int main(){
+    int num;
     while(cin>>num){
-    cout<<count0(num);
-    for(int i=1;i<10;i++){
-    cout<<" "<<count(num,i);}
-    cout<<endl;}
+    cout<<countnum(num,0);
+    for(int i=1;i<10;i++)
+        cout<<" "<<countnum(num,i);}
     return 0;
 }
