e61a0ec0eadf3d87f77ca1f236914a0367a72504
daibigmonster <546508148@qq.com>
2017-08-19 00:07:24 +0800

    1708191

--- Prepare4work/173_schoolEnroll/3.cpp
+++ Prepare4work/173_schoolEnroll/3.cpp
@@ -4,17 +4,23 @@ using namespace std;
 int compare(string A,string B)
 {
     int lenA=A.size(),lenB=B.size();
-    if(B.empty()||lenA>lenB)return 0;
+    int N=lenB-lenA,count=0,max=0;
+    if(B.empty()||lenA>lenB)return -1;
     if(A.empty())return lenB;
-    for(int i=0;i<lenA;i++){
-        for(int j=0;j<lenB;j++){
-            if(A[i]==B[j])}}
+    for(int i=0;i<=N;i++){
+        for(int j=0;j<lenA;++j){
+            if(B[i+j]==A[j])
+                count++;
+        }
+    max=max>count?max:count; 
+    count=0;
     }
+    return lenA-max;}
 int main(){
     string A;
     string B;
     cin>>A>>B;
-    cout<<compare(A,B);
+    cout<<compare(A,B)<<endl;
     return 0;
 }
 
