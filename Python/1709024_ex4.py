08415dcd2b38e79ceb7d421bf699af68cecd40b7
daibigmonster <546508148@qq.com>
2017-09-02 22:16:21 +0800

    170902晚上

--- Python/1709024_ex4.py
+++ Python/1709024_ex4.py
@@ -5,3 +5,12 @@ passengers=90
 cars_not_drivers=cars-drivers
 cars_driven=drives
 carpool_capacity=cars_drivern*space_in_a_car
+average_passengers_per_car=passengers/cars_driven
+
+
+print ("There are"),cars,"cars available."
+print "There are only",drivers,"drivers available."
+print "There will be",cars_not_driven,"drivers available."
+print "We can tansport",carpool_capacity,"people today."
+print "We have",passengers,"to carpool today."
+print "We need to put about",average_passengers_per_car,"int each car."
