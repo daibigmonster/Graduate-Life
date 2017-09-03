d302c9e4c645c6dd0ac5eac42239ec748e68e965
daibigmonster <546508148@qq.com>
2017-09-03 12:05:58 +0800

    170903

--- Python/1709024_ex4.py
+++ Python/1709024_ex4.py
@@ -2,9 +2,9 @@ cars=100;
 space_in_a_car=4.0
 drivers=30
 passengers=90
-cars_not_drivers=cars-drivers
-cars_driven=drives
-carpool_capacity=cars_drivern*space_in_a_car
+cars_not_driven=cars-drivers
+cars_driven=drivers
+carpool_capacity=cars_driven*space_in_a_car
 average_passengers_per_car=passengers/cars_driven
 
 
