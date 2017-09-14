4c17bf77c1d453eb4f0c8bd55d5b060c0d520441
daibigmonster <546508148@qq.com>
2017-09-14 23:54:55 +0800

    170914学习

--- Prepare4work/1709141_surrounded_regions.cpp
+++ Prepare4work/1709141_surrounded_regions.cpp
@@ -18,9 +18,41 @@
  X O X X
 */
 int rowNum=0;
-int colNum-0;
+int colNum=0;
 public void solve(char[][] board){
 	if(boad==null||board.length<=0||board[0].length<=0){
 		return;
 	}
-	rowNum=board.length;}
+	rowNum=board.length;
+	colNum=board[0].length;
+	for(int i=0;i<colNum,i++){
+		dfs(board,0,i);
+		dfs(board,rowNum-1,i);
+	}
+	for(int i=0;i<rowNum;i++){
+		dfs(board,i,0);
+		dfs(board,i,colNum-1);
+	}
+	for(int i=0;i<rowNum;i++){
+		for(int j=0;j<colNum;j++){
+			if(board[i][j]=='*'){
+				board[i][j]='O';
+			else
+				board[i][j]='X';
+			}
+		}
+	}
+}
+void dfs(char[][] board.int row,int col){
+	if(board[row][col]=='O'){
+		board[row][col]='*';
+		if(row>1){
+			dfs(board,row-1,col)}
+		if(col>1)
+			dfs(board,row,col-1)
+		if(row<rowNum-1)
+			dfs(board,row+1,col);
+		if(col<colNum-1)
+			dfs(board,row,col+1);
+	}
+}
