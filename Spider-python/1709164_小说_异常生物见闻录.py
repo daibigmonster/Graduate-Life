c304cb8dbe8239d0ad745edd254503707d54c4fd
daibigmonster <546508148@qq.com>
2017-09-17 01:40:05 +0800

    170917

--- Spider-python/1709164_小说_异常生物见闻录.py
+++ Spider-python/1709164_小说_异常生物见闻录.py
@@ -16,17 +16,18 @@ def make_list(html):
 	items=re.findall(pattern,html)
 	conn=sqlite3.connect('/home/dai/Graduate-Life/Backup/countnum.db')
 	cur=conn.cursor()
-	cur.execute('create table if not exists booklist (name varchar(30),address varchar(50) primary key)')
+	cur.execute('create table if not exists 异常生物见闻录 (name varchar(30),address varchar(50) primary key)')
+#需要改成小说名字
 	for item in items:
 		#print(item)
 		url=base_url+item[0]
-		cur.execute('select * from booklist where address=?',(url,))
+		cur.execute('select * from 异常生物见闻录 where address=?',(url,))#需要改成小说名字
 		values=cur.fetchall()
 		if values==[]:
-			cur.execute('insert into booklist (name,address) values (?,?)',(item[1],url))
+			cur.execute('insert into 异常生物见闻录 (name,address) values (?,?)',(item[1],url))#需要改成小说名字
 		else:
 			pass
-	cur.execute('select * from booklist')
+	cur.execute('select * from 异常生物见闻录')#需要改成小说名字
 	values=cur.fetchall()
 	cur.close()
 	conn.commit()
@@ -49,7 +50,7 @@ def main():
 		novelname='/home/dai/文档/'+novelname
 		if not os.path.exists(novelname):
 			os.system('mkdir {}'.format(novelname))
-	first_url='http://www.xs.la/1_1212/'#小说的首页，从首页就可以抓取目录
+	first_url='http://www.xs.la/3_3271/'#小说的首页，从首页就可以抓取目录
 	sourcecode=get_one_page(first_url)
 	values=make_list(sourcecode)
 #	print(novelname)
@@ -61,7 +62,7 @@ def main():
 		if not os.path.exists(bookpath):
 			sourcecode=get_one_page(item[1])
 			parse_one_page(bookpath,sourcecode)
-			time.sleep(0.2)
+			time.sleep(0.1)
 		else:
 			pass
 
