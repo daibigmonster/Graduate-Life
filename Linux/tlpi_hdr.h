739b202f86e424562992c54b16a226b4e09f0f6d
daibigmonster <546508148@qq.com>
2017-10-18 19:51:53 +0800

    171018

---
+++ Linux/tlpi_hdr.h
@@ -0,0 +1,11 @@
+#ifndef TLPI_HDR_H
+#define TLPI_HDR_H
+
+#include <sys/types.h> //Types definitions used by many programs
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //Prototypes for many system calss
+#include <errno.h> //Declares errno and defines error constants
+#include <string.h> //Commonly used by string-handling functions
+
+#include "get_num.h" //De
