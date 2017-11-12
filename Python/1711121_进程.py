c2c017b66780fb0c74c0b199c12a2b1cc10dd248
daibigmonster <546508148@qq.com>
2017-11-12 14:33:46 +0800

    关于python的学习

--- Python/1711121_进程.py
+++ Python/1711121_进程.py
@@ -1,6 +1,16 @@
+from multiprocessing import Process
 import os
-print('进程 (%s) 开始...' % os.getpgid())
-pid = os.fork()
-if pid == 0 :
-    print('I am child process (%s) and my parent is %s.' % (os.getpid(),\
-    os.getppid()))
+
+def run_proc(name) :
+    print('Run child process %s (%s)...' % (name,os.getpid()))
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target = run_proc,args = ('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
+#multiprocessing 模块就是跨平台版本的多进程模块
+#multiprocessing 模块提供了一个Process类来代表一个进程对象
+#
