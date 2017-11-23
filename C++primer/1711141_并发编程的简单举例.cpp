e5c85dc64c7215ad0b876a6687c4dc9d3d4a5fa9
daibigmonster <546508148@qq.com>
2017-11-23 23:54:23 +0800

    max

--- C++primer/1711141_并发编程的简单举例.cpp
+++ C++primer/1711141_并发编程的简单举例.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include <thread>
-
 using namespace std;
 
 void hello(){
@@ -14,7 +13,7 @@ int main(){
 	cout << "MainThread is : " << this_thread::get_id() << endl;
 	thread t(hello);
 	cout << t.get_id() << endl;
-	cout << hex << t.get_id() << endl;
+	cout << std::oct << t.get_id() << endl;
 	t.join();
 	return 0;
 }
