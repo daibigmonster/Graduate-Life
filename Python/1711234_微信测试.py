6eb432dbee7686a4721b03c01eb40308b74f0b64
daibigmonster <546508148@qq.com>
2017-11-24 23:39:06 +0800

    微信机器人的修改

--- Python/1711234_微信测试.py
+++ Python/1711234_微信测试.py
@@ -3,8 +3,7 @@ from wxpy import *
 bot = Bot()
 
 GF = bot.friends().search('管小洋',sex = FEMALE)[0]
-GF.send('戴涧微信机器人上线测试')
-GF.send('让我们开始聊天吧！')
+GF.send('戴爱羊来啦')
 
 tuling = Tuling(api_key='d68ebf6b345a4b2c9a3ed4e35e045e55')
 
