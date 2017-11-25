d47d3d99a7612bcce439cf952652e2d622e0fa81
daibigmonster <546508148@qq.com>
2017-11-25 19:03:56 +0800

    仍然是对爬虫的学习

--- Spider-python/1711211_爬虫测试.py
+++ Spider-python/1711211_爬虫测试.py
@@ -1,24 +1,11 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-import requests
-from requests.exceptions import RequestException
-
-def get_one_page(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
-}
-    try:
-        response = requests.get(url,headers=headers,timeout=10)
-        if response.status_code == 200:
-       	    return response
-        return None
-    except RequestException:
-        return None
 
 
 def getContext(url):
     try:
-        html = get_one_page(url)
+        html = urlopen(url)
     except HTTPError as e:
         return None
     try:
