afd0195b6ba889275e073156a476234e802fc3cb
daibigmonster <546508148@qq.com>
2017-12-09 22:14:43 +0800

    今天js学习

--- Web/1712091_hello.js
+++ Web/1712091_hello.js
@@ -2,4 +2,24 @@
 var  name = 'Dai Jian';
 console.log(`Hello,${name}!`);
 
-var 
+var xiaoyang = {
+    name : '小羊羊',
+    birth : 1991,
+    school : 'Songlei Middle school',
+    height : 1.75,
+    weitght : 58,
+    score : 95
+
+};
+
+console.log(xiaoyang.name);
+console.log(xiaoyang.birth);
+console.log(xiaoyang.school);
+console.log(xiaoyang.weitght);
+
+var arry = ['dai','guan','baidu','alibaba','tencent']
+for(var key in arry){
+    console.log(key);
+    console.log(arry[key]);
+
+}
