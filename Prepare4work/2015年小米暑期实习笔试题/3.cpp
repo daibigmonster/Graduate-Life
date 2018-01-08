86b7c53722f80355e0799344d90b29f1752bc168
daibigmonster <546508148@qq.com>
2018-01-08 21:36:52 +0800

    把小米题目做完

--- Prepare4work/2015年小米暑期实习笔试题/3.cpp
+++ Prepare4work/2015年小米暑期实习笔试题/3.cpp
@@ -20,33 +20,68 @@
 #include <vector>
 #include <map>
 using namespace std;
-int getParent(string A){
-    
+int getsameparent(vector<string> matrix,int indexA,int indexB);
+map<int,int> recoder;
+int getParent(vector<string> matrix,int a){
+    int length = matrix[a].size();
+    for(int i = 0;i < length;i++){
+        if(matrix[a][i] == '1' && (recoder[i] == recoder[a] - 1)) return i;
+    }
+    return -1;
+}
+
+void DFSmatrix(vector<string> matrix,int i,int col,int level){
+    vector<int> num;
+    recoder[i] = level + 1;
+    for(int j = 0;j < col;j++){
+        if(matrix[i][j] == '1'){
+            if(recoder.count(j) == 0)
+                num.push_back(j);
+        }
+    }
+    for(int i : num)DFSmatrix(matrix,i,col,level + 1);
 }
 int getSplitNode(vector<string> matrix, int indexA, int indexB) {
     if(indexA == 0 || indexB == 0) return 0;
     int row = matrix.size();
     int col = matrix[0].size();
-    map<int,int> recoder;
-    recoder[0] = 0;
-    for(int i = 0;i < row;i++){
-        for(int j = i + 1;j < col;j++){
-            if(matrix[i][j] == '1')recoder[j] = recoder[i] + 1;
-        }
-    }
-    if(recoder[indexA] == recoder[indexB]){
-        for(auto:)
-    }
-    // for(auto it = recoder.begin();it != recoder.end();it++){
-    //     cout << " " << (*it).first << " " << (*it).second << endl;
+    DFSmatrix(matrix,0,col,-1);
+    // for(int i = 0;i < row;i++){
+    //     for(int j = i + 1;j < col;j++){
+    //         if(matrix[i][j] == '1')recoder[j] = recoder[i] + 1;
+    //     }
     // }
-    // return 0;
+    if(recoder[indexA] == recoder[indexB])return getsameparent(matrix,indexA,indexB);
+    else if(recoder[indexA] > recoder[indexB]){
+        while(recoder[indexA] != recoder[indexB])indexA = getParent(matrix,indexA);
+        return getsameparent(matrix,indexA,indexB);
+    }
+    else{
+        while(recoder[indexA] != recoder[indexB]){
+            indexB = getParent(matrix,indexB);
+            // cout << indexB << endl;
+        }
+        return getsameparent(matrix,indexA,indexB);
+    }
+}
+
+int getsameparent(vector<string> matrix,int indexA,int indexB){
+    if(indexA == indexB) return indexA;
+    while(getParent(matrix,indexA) != getParent(matrix,indexB)){
+        indexA = getParent(matrix,indexA);
+        indexB = getParent(matrix,indexB);
+    }
+    return getParent(matrix,indexA);
 }
 
 int main(){
-    string str[5] = {"01011","10100","01000","10000","10000"};
-    vector<string> matrix(str,str + 5);
-    getSplitNode(matrix,1,2);
+    // [0000001001,0010100000,0100000000,0000000011,0100000010,0000000001,1000000000,0000000001,0001100000,1001010100],6,2
+    string str[10] = {"0000001001","0010100000","0100000000","0000000011","0100000010","0000000001","1000000000","0000000001","0001100000","1001010100"};
+    vector<string> matrix(str,str + 10);
+    cout << getSplitNode(matrix,6,2) << endl;
+    for(auto it = recoder.begin();it != recoder.end();it++){
+        cout << " " << (*it).first << " " << (*it).second << endl;
+    }
     return 0;
     
 }
