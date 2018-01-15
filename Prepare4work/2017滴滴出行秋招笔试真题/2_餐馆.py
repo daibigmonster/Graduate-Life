a28f10aa82eecb032a076c1dfd6144e4d4c9bf65
daibigmonster <546508148@qq.com>
2018-01-16 02:53:56 +0800

    又是一道题

--- Prepare4work/2017滴滴出行秋招笔试真题/2_餐馆.py
+++ Prepare4work/2017滴滴出行秋招笔试真题/2_餐馆.py
@@ -3,7 +3,9 @@
 
 # 输入包括m+2行。 第一行两个整数n(1 <= n <= 50000),m(1 <= m <= 50000) 第二行为n个参数a,即每个桌子可容纳的最大人数,以空格分隔,范围均在32位int范围内。 接下来m行，每行两个参数b,c。分别表示第i批客人的人数和预计消费金额,以空格分隔,范围均在32位int范围内。
 
-
+# n,m
+# a * n
+# b * m,c * m
 
 # 输出描述:
 
@@ -12,11 +14,33 @@
 
 # 输入例子1:
 
-# 3 5 2 4 2 1 3 3 5 3 7 5 9 1 10
+# 3 5 
+# 2 4 2 
+# 1 3 
+# 3 5 
+# 3 7 
+# 5 9 
+# 1 10
 
 
 # 输出例子1:
 
 # 20
-import sys
-m,n = sys.stdin.readline().strip().split()
+import bisect
+ 
+n,m = map(int,input().split())
+a = sorted(list(map(int,input().split())))
+M = []
+for i in range(m):
+    M.append(list(map(int,input().split())))
+ 
+Mc = sorted(M,key = lambda x : x[1],reverse = True)
+sum = 0
+for l in Mc:
+    index = bisect.bisect_left(a,l[0])
+    if index < len(a):
+        sum += l[1]
+        del a[index]
+ 
+print(sum)
+
