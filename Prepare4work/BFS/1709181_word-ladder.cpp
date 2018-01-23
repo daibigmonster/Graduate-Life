518ae5d80805d1bcfe74dc6084dccaa1d2a4ae75
daimonster <546508148@qq.com>
2018-01-24 01:44:20 +0800

    11

--- Prepare4work/BFS/1709181_word-ladder.cpp
+++ Prepare4work/BFS/1709181_word-ladder.cpp
@@ -1,25 +1,25 @@
 
 
-Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
+// Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from start to end, such that:
 
-    Only one letter can be changed at a time
-    Each intermediate word must exist in the dictionary 
+//     Only one letter can be changed at a time
+//     Each intermediate word must exist in the dictionary 
 
-For example,
+// For example,
 
-Given:
-start ="hit"
-end ="cog"
-dict =["hot","dot","dog","lot","log"]
+// Given:
+// start ="hit"
+// end ="cog"
+// dict =["hot","dot","dog","lot","log"]
 
-As one shortest transformation is"hit" -> "hot" -> "dot" -> "dog" -> "cog",
-return its length5.
+// As one shortest transformation is"hit" -> "hot" -> "dot" -> "dog" -> "cog",
+// return its length5.
 
-Note:
+// Note:
 
-    Return 0 if there is no such transformation sequence.
-    All words have the same length.
-    All words contain only lowercase alphabetic characters. 
+//     Return 0 if there is no such transformation sequence.
+//     All words have the same length.
+//     All words contain only lowercase alphabetic characters. 
 
 class Solution {
 public:
