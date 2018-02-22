e03ac217cb5addc7ecc43258dd9bea606b33f2ea
daimonster <546508148@qq.com>
2018-02-22 12:14:11 +0800

    php first commit

---
+++ Prepare4work/17年校招全国统一模拟第四场/6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(cin >> n)
+    {
+        vector<int> h_i(n);
+        for(int i = 0;i < n;i++)
+        {
+            cin >> h_i[i];
+        }
+        deque<int> leastheightdiff;
+        int res = 0;
+        sort(h_i.begin(),h_i.end());
+        for(int i = n - 1;i >= 0;i--)
+        {
+            if(i%2) leastheightdiff.push_back(h_i[i]);
+            else leastheightdiff.push_front(h_i[i]);
+        }
+        leastheightdiff.push_back(leastheightdiff.front());
+        for(int i = 0;i < n;i++)
+        {
+            res = max(res,abs(leastheightdiff[i + 1] - leastheightdiff[i]));
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
