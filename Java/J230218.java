6496fea4d04dd4d1181a9ecbe80b52f3f1b6ec17
daimonster <546508148@qq.com>
2018-02-23 23:00:22 +0800

    学习java

---
+++ Java/J230218.java
@@ -0,0 +1,19 @@
+/**
+ * 抽象类与接口的应用
+ */
+
+abstract class A
+{
+    public abstract void print();
+}
+class B extends A
+{
+    public void print()
+    {
+        System.out.println("Hello World!!!");
+    }
+    
+};
+public class AVname() {
+    
+}
