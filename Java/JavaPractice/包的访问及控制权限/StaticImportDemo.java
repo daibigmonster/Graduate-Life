adcb0a538d37c807222a0362abd25afb5ea600a6
daimonster <546508148@qq.com>
2018-03-02 17:01:48 +0800

    包控制的学习

---
+++ Java/JavaPractice/包的访问及控制权限/StaticImportDemo.java
@@ -0,0 +1,15 @@
+package dai.f;
+import static dai.e.Operate.*;
+/**
+ * StaticImportDemo
+ */
+public class StaticImportDemo {
+
+    public static void main(String[] args) {
+        System.out.println("3 + 3 = " + add(3,3));
+        System.out.println("3 - 2 = " + sub(3,2));
+        System.out.println("3 * 3 = " + mul(3,3));
+        System.out.println("3 / 3 = " + div(3,3));
+
+    }
+}
